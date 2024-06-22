1b4abdf403d34574639453ba414bc0ed10bb9557
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2024-06-22 11:53:45 +0300

    update

--- 01_lesson/main.py
+++ 01_lesson/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('hello.html')
+    return render_template('/hello.html')
 
 @app.route('/students')
 def students():
