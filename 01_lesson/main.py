cfd57de67b501587fc0f1886dcb744cf93724aa6
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2024-06-22 11:49:04 +0300

    Initial commit

---
+++ 01_lesson/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('hello.html')
+
+@app.route('/students')
+def students():
+    students = [
+        ('Yaroslav', 'student'),
+        ('Milan', 'student'),
+        ('Maksym', 'student')
+    ]
+    return render_template('students.html', students=students)
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
+
+
+
