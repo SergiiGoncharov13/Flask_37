1d10eeb4b525f16bfd83a90682fe1938ab0e63d1
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2024-10-05 09:55:53 +0300

    base, update functions

--- flask_travels/app.py
+++ flask_travels/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import data
 
 
 app = Flask(__name__)
@@ -6,16 +7,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html', 
+        departures=data.departures,
+        title=data.title,
+        subtitle=data.subtitle,
+        description=data.description,
+        tours=data.tours
+        )
+
+
+@app.route('/departures/<departure>')
+def departure(departure):
+    tours = dict(filter(lambda tour: tour[1]['departure'] == departure, data.tours.items()))
+    if tours:
+        return render_template('departure.html',
+                departure=departure,
+                title=data.title,
+                departures=data.departures,
+                tours=data.tours)
+    else:
+        abort(404)
+
+@app.route('/departures')
+def departures_zero():
     return render_template('index.html')
 
-
-@app.route('/departure')
-def departure():
-    return render_template('departure.html')
-
-@app.route('/tours')
-def list_tours():
-    return render_template('tour.html')
+    
+@app.route('/tours/<int:id>')
+def list_tours(id):
+    return render_template('tour.html',
+        tour=data.tours[id],
+        title=data.title,
+        departures=data.departures)
 
 
 
--- flask_travels/data.py
+++ flask_travels/data.py
@@ -1,14 +1,14 @@
 title = "Flask Travel"
 subtitle = "Для відпочинку"
 description = "Найпопулярніші напрямки "
-departures = {"kuiv": " з Києва", "lviw": " зі Львова", "odessa": " з Одеси", "chernivtsi": " з Чернівець",
+departures = {"kyiv": " з Києва", "lviv": " зі Львова", "odessa": " з Одеси", "chernivtsi": " з Чернівець",
               "chernigiv": " з Чернігова"}
 
 tours = {
     1: {
         "title": "Закарпаття",
         "description": "затишний готель.",
-        "departure": "kuiv",
+        "departure": "kyiv",  # Corrected from "kuiv"
         "picture": "https://zahidkurort.com.ua/wp-content/themes/kurort/cache/45/03cd84cf64a6145_1013x532.jpg",
         "price": 62000,
         "stars": "4",
@@ -18,8 +18,8 @@ tours = {
     },
     2: {
         "title": "Батяр",
-        "description": "Будинок готелю має форму короткої літери U. ",
-        "departure": "lviw",
+        "description": "Будинок готелю має форму короткої літери U.",
+        "departure": "lviv",  # Corrected from "lviw"
         "picture": "https://bukovel-hotel-ua.hotelmix.com.ua/data/Photos/OriginalPhoto/11709/1170996/1170996471/Bukovel-Hotel-Exterior.JPEG",
         "price": 85000,
         "stars": "5",
@@ -29,7 +29,7 @@ tours = {
     },
     3: {
         "title": "Ліжник",
-        "description": "Готель з білого каменю",
+        "description": "Готель з білого каменю.",
         "departure": "odessa",
         "picture": "https://cf.bstatic.com/xdata/images/hotel/max500/266741392.jpg?k=8ca8f1ce6b54ff7d7bc5316c82f3d15aada16834b1b1108d8e79edca5d60c7b5&o=&hp=1",
         "price": 63000,
@@ -40,8 +40,8 @@ tours = {
     },
     4: {
         "title": "Ведмежа",
-        "description": "Красивий краєвид з вікна",
-        "departure": "kuiv",
+        "description": "Красивий краєвид з вікна.",
+        "departure": "kyiv",  # Corrected from "kuiv"
         "picture": "https://karpatskyi-teremok-dzembronia.hotelmix.com.ua/data/Photos/OriginalPhoto/12654/1265492/1265492017/Karpatskyi-Teremok-Exterior.JPEG",
         "price": 62000,
         "stars": "4",
@@ -51,7 +51,7 @@ tours = {
     },
     5: {
         "title": "Сова",
-        "description": "Розкішні апартаменти на березі водосховища",
+        "description": "Розкішні апартаменти на березі водосховища.",
         "departure": "chernivtsi",
         "picture": "https://pravda.if.ua/wp-content/uploads/2020/04/10827878_1539108119699451_4179360364797503328_o.jpg",
         "price": 68000,
@@ -62,7 +62,7 @@ tours = {
     },
     6: {
         "title": "Верховина",
-        "description": "в центрі міста",
+        "description": "в центрі міста.",
         "departure": "chernigiv",
         "picture": "https://static.karpaty.ua/uploads/house/55cc5c27c0557c2ebc003185/main_photo/big.jpg",
         "price": 53000,
@@ -73,7 +73,7 @@ tours = {
     },
     7: {
         "title": "Піп-Іван",
-        "description": "З готелю близько до Чорногори",
+        "description": "З готелю близько до Чорногори.",
         "departure": "odessa",
         "picture": "https://q-xx.bstatic.com/xdata/images/hotel/max500/300980475.jpg?k=a26871869739483c09ac4493f59c337ec2bc8acc5acd96d1b141b0f184399734&o=",
         "price": 72000,
@@ -84,15 +84,16 @@ tours = {
     },
     8: {
         "title": "Купель",
-        "description": "термальні води",
-        "departure": "kuiv",
+        "description": "термальні води.",
+        "departure": "kyiv",  # Corrected from "kuiv"
         "picture": "https://turua.com.ua/upload/iblock/b6d/b6d7e9601f754931b633b8be6e10afd3.jpg",
         "price": 44000,
         "stars": "4",
         "country": "Велятино",
         "nights": 7,
         "date": "21 січня",
-    },
-
-
+    }
 }
+
+
+
