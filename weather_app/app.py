fc10ca5acbab3f6eaa34f2af7bd50a8d694d6f5a
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2024-10-11 20:44:05 +0300

    Wether_app

---
+++ weather_app/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, jsonify
+
+import requests
+
+openweather_api_key = "62319bde120f4537451f6a6f4592956f"
+
+
+app = Flask(__name__)
+
+
+def extract_weather_data(weather_data):
+   main = weather_data['main']
+   extracted_data = {
+       'temp': main['temp'],
+       'feels_like': main['feels_like'],
+       'temp_min': main['temp_min'],
+       'temp_max': main['temp_max'],
+       'weather_main': weather_data['weather'][0]['main'],
+       'weather_description': weather_data['weather'][0]['description'],
+       'wind_speed': weather_data['wind']['speed'],
+       'clouds': weather_data['clouds']['all'],
+       'dt_txt': weather_data['dt_txt']
+   }
+   return extracted_data
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_weather')
+def get_weather():
+   location = request.args.get('location')
+   if location:
+       url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&APPID={openweather_api_key}'
+       response = requests.get(url)
+       data = response.json()
+       return jsonify(data)
+   else:
+       return jsonify(error='Location not provided'), 400
+
+
+@app.route('/get_weather_forecast')
+def get_weather_forecast():
+   location = request.args.get('location')
+   if location:
+       url = f'http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={openweather_api_key}&units=metric'
+       response = requests.get(url)
+       data = response.json()
+
+       # get weather info for nearest few days
+       forecast = []
+       for entry in data['list']:
+           extracted_data = extract_weather_data(entry)
+           forecast.append(extracted_data)
+       return jsonify(forecast)
+   else:
+       return jsonify(error='Location not provided'), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+
+
